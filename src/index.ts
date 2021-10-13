90e9ac733143dd17b46be2ba0eb045c823ef1e2c
Syncxv <aruldevarackal1@gmail.com>
2021-10-14 03:37:48 +0530

    got the sort to work on the role array

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { Client, Intents, Message } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 import config from '../config.json'
-const getGuild = async () => {
-    return await client.guilds.fetch("770554369707343884")
-}
 client.on('ready', () => {
     console.log("ready as ", client.user?.username)
 })
 client.on('interactionCreate', async (e) => {
     if(e.isMessageComponent()) {
+        if(e.customId = "")
         if(e.customId === "role-select" && e.componentType === "SELECT_MENU") {
             console.log(e)
             if(!e.guildId || !e.member) return console.log("no member or guild")
@@ -17,9 +15,8 @@ client.on('interactionCreate', async (e) => {
             if(!member) return console.log("no member")
             let role = guild.roles.cache.find(r => r.id == e.values[0]);
             if(!role) return console.log("no role")
-            console.log(guild, member, role);
             member.roles.add(role);
-            e.reply("DONE")
+            e.reply(":D")
            
         }
     }
@@ -29,7 +26,19 @@ client.on('messageCreate', async (message: Message) => {
     if(message.content === "!hehe") {
         if(!message.guildId) return
         const guild = await client.guilds.fetch(message.guildId)
-        console.log(guild)
+        //guild.roles.cache is a map so we need to convert it to an array. then we need to sort it by the postion it is on discord so we can get the correct indicies hehee
+        const rolesArray = [...guild.roles.cache].map(([id, role, ...shesh]) => ({ id, role, ...shesh })).sort((a, b) => {
+            if(a.role.position < b.role.position) return 1
+            else return -1
+        })
+        //ALL the color roles are next to each other so we just have to slice them of the rest :D so we getting the index of these and then we slice
+        const firstColorRole = rolesArray.map(e => e.role.name).indexOf("Burgundy Red")
+        const lastColorRole = rolesArray.map(e => e.role.name).indexOf("IndianRed")
+        console.log(firstColorRole, lastColorRole)
+        const hehe = rolesArray.slice(firstColorRole, lastColorRole)
+        console.log(hehe);
+        (global as any).rolesArray = rolesArray;
+        (global as any).guild = guild;
         message.channel.send({
             content: "bruh",
             components: [
