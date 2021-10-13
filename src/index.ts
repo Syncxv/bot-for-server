5dbf4cdf5a46879e060ec8c33ca3fa680fcc6504
Syncxv <aruldevarackal1@gmail.com>
2021-10-14 01:41:02 +0530

    component is working :D

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
-import { Client, Intents } from 'discord.js';
+import { Client, Intents, Message } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 import config from '../config.json'
 
-
 client.on('ready', () => {
     console.log("ready as ", client.user?.username)
-} )
-
+})
+client.on('interaction', (e) => {
+    console.log(e)
+})
+client.on('messageCreate', (message: Message) => {
+    if(message.content === "!hehe") {
+        message.channel.send({
+            content: "bruh",
+            components: [
+                {
+                    type: "ACTION_ROW",
+                    components: [
+                        {
+                            type: "SELECT_MENU",
+                            customId: "role-select",
+                            options: [
+                                {
+                                    label: "Nigga Role",
+                                    value: "897938535762522183"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        })
+    }
+})
 client.login(config.token);
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+// enum MessageComponentTypes {
+//     ACTION_ROW = "ACTION_ROW",
+//     BUTTON = "BUTTON",
+//     SELECT_MENU ="SELECT_MENU"
+// }
